7280a72709af38f155027ab33bc25b9fe36855d5
rmdcoelho <rmdcoelho@gmail.com>
2024-01-25 22:38:28 -0300

    Fix sqlite3 and api-inventory

--- src/data/sqlite3/init.sql
+++ src/data/sqlite3/init.sql
@@ -1,7 +1,9 @@
-CREATE TABLE inventory IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS inventory(
 	id int primary key,
 	description varchar(250) NOT NULL
 );
+
+DELETE FROM inventory;
 
 INSERT INTO inventory (id, description) VALUES (1, 'Copo');
 INSERT INTO inventory (id, description) VALUES (2, 'Batata');
